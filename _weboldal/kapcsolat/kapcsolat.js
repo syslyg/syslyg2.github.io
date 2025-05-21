ec25695075b5d69fcd1a020a2e4dbc56000e3603
syslyg <ritz.andras13@gmail.com>
2025-05-21 02:34:00 +0200

    debug stuff (működik)

--- _weboldal/kapcsolat/kapcsolat.js
+++ _weboldal/kapcsolat/kapcsolat.js
@@ -1,3 +1,9 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+  document.getElementById("formMessage").textContent = "Köszönöm az üzenetet! Hamarosan jelentkezem.";
+  this.reset();
+});
+
 function closeWindow() {
     window.close();
 }
--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-  e.preventDefault();
-  document.getElementById("formMessage").textContent = "Köszönöm az üzenetet! Hamarosan jelentkezem.";
-  this.reset();
-});
-
 function openKapcsolat() {
   const width = 600;
   const height = 600;
@@ -12,8 +6,62 @@ function openKapcsolat() {
   const top = (screen.height - height) / 2;
 
   window.open(
-    "kapcsolat.html",
+    "_weboldal/kapcsolat/kapcsolat.html",
     "KapcsolatAblak",
     `width=${width},height=${height},left=${left},top=${top},resizable=no`
   );
 }
+
+let aktualisFajl = "";
+
+function betolt(fajlNev) {
+  fetch(fajlNev)
+    .then(response => {
+      if (!response.ok) throw new Error("Hiba történt a fájl betöltésekor.");
+      return response.text();
+    })
+    .then(szoveg => {
+      document.getElementById("tartalom").innerHTML = szoveg;
+      aktualisFajl = fajlNev;
+
+      const homeGomb = document.getElementById("homeBtn");
+      if (aktualisFajl === "_weboldal/home.html") {
+        homeGomb.style.display = "none";
+      } else {
+        homeGomb.style.display = "inline-block";
+      }
+      const projektDiv = document.getElementById("projekt");
+        projektDiv.innerHTML = "";
+        projektDiv.classList.remove("vanSzoveg");
+    })
+    .catch(error => {
+      document.getElementById("tartalom").innerHTML = "Nem sikerült betölteni az adatokat.";
+      console.error(error);
+    });
+}
+
+function projekt_betolt(fajlNev) {
+  const projektDiv = document.getElementById("projekt");
+
+  fetch(fajlNev)
+    .then(response => {
+      if (!response.ok) throw new Error("Hiba történt a fájl betöltésekor.");
+      return response.text();
+    })
+    .then(szoveg => {
+      document.getElementById("projekt").innerHTML = szoveg;
+      if (szoveg.trim() !== "") {
+        projektDiv.classList.add("vanSzoveg");
+        } else {
+        projektDiv.classList.remove("vanSzoveg");
+        }
+    })
+    .catch(error => {
+      document.getElementById("projekt").innerHTML = "Nem sikerült betölteni az adatokat.";
+      console.error(error);
+    });
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  betolt("_weboldal/home.html");
+});
